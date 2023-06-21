1313164a51bae424680a3723dafef95f15feeb53
lewisbass <lewisbass@vt.edu>
2023-06-21 16:21:52 -0400

    misc

--- dicts/amino_44/pdb2smiles.py
+++ dicts/amino_44/pdb2smiles.py
@@ -2,6 +2,7 @@
 from rdkit import Chem
 import os
 import pandas as pd
+import pickle
 
 if('pdb' not in os.listdir()):
     print('could not find pdb folder')
@@ -10,16 +11,21 @@ if('pdb' not in os.listdir()):
 tip_file = pd.read_csv('TI_amino_48.dat', sep=' ', header=None).to_numpy()
 tip_dict = {}
 for i in tip_file:
-    tip_dict[i[0]] = i[1]
+    tip_dict[i[0]] = {}
+    tip_dict[i[0]]['tip3p'] = i[1]
 
-os.chdir('pdb')
-for f in os.listdir():
-    mol = Chem.rdmolfiles.MolFromPDBFile(f)
+
+for f in os.listdir('pdb'):
+    mol = Chem.rdmolfiles.MolFromPDBFile('pdb/'+f)
     n = f.replace('.pdb', '')
+    smiles = Chem.rdmolfiles.MolToSmiles(mol)
     if(n not in tip_dict.keys()):
         print("missing tip3p value for " + n)
         continue
-    print(n, tip_dict[n], Chem.rdmolfiles.MolToSmiles(mol))
+    print((' '+n+" ")[-9:], (str(tip_dict[n]['tip3p'])+'    ')[:8], smiles)
+    tip_dict[n]['smiles'] = smiles
 
+
+pickle.dump(tip_dict, open("amino_smiles.pickle", 'wb'))
 
 
